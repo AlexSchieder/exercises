13a2ae999625717c9cf793433e94c6b63ae55158
AlexSchieder <50061502+AlexSchieder@users.noreply.github.com>
2019-11-14 15:26:08 +0100

    Add new code and new file for exercise description

--- exercise_2.php
+++ exercise_2.php
@@ -1,45 +1,44 @@
-<?
-/* counting words
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?
 
-Write a function 'countWords($str)' that takes any string of characters
-and finds the number of times each word occurs. You should ignorethe distinction
-between capital and lowercase letters, and do not have to worry about dealing
-with characters that are not letters.
-
-Hint: Create an associative array mapping word keys to the number of times they occur.
-You will need to look at PHP's string functions to split a sentence into words.
-
-Hint 2: The print_r($array_name) function is useful for examining the contents
-of an array.*/
-
-$sentence = "Here is a quick cheat sheet of the main PHP regex functions.";
+//$sentence = "Here is a quick cheat sheet of the main PHP regex functions.";
 
 function countWords($str) {
   $str_1 = strtolower($str);
-  $str_new = preg_replace('/[.,;:- !]/', '', $str_1);
+  $str_new = preg_replace('/[.,;:\-!\s]/', '', $str_1);
   $words = explode(" ", $str_new);
   //print_r($words);
 
-  $new = array_count_values($words);
-  print_r($new);
+  return array_count_values($words);
 
 }
-countWords($sentence);
 
-/*
-in the same file, write a form consisting of a single text field and a submit button.
-The "action" attribute to the form sjould be the same page that the form is on
-(don't hard code, use $_SERVER['PHP_SELF']). The form should send the contents of
-text field via GET.
+$array = countWords($str);
+//countWords($sentence);
 
-Upon submitting the form, you should be redirected to the same page, but the URL
-should contain the string from the text field as a GET request normally behaves.
 
-Side note: Prevent security vulnerabilies with $_SERVER['PHP_SELF'].
-*/
+  if(isset($_GET['submit'])) {
+    //echo $_GET['text'];
+    countWords($_GET['text']);
+  }
 
-/* CHeck for the existence of a GET request. If a GET request exists with the name of the
-input field that you made before, run the contents of the request through your
-countWords($str) function. Take the output array of the function, and display an HTML
-table of words and the number of times they occur. Make the table sorted by decreasing
-number of occurences.*/
+?>
+  <form action="<?$_SERVER['PHP_SELF']?>" method="get">
+    <input type="text" name="text" value="">
+    <input type="submit" name="submit" value="submit">
+  </form>
+  <table>
+    <?
+    foreach ($array as $key => $value) {
+      echo "<tr><td>". $key . "</td><td>" . $value . "</td></tr>";
+    }
+    ?>
+  </table>
+</body>
+</html>
